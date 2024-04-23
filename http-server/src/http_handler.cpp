0ec48771ef60a388e54975aa3a301a33d6685b3f
starodubov <s.s.starodubov@yandex.ru>
2024-04-23 22:16:06 +0400

    http-server

--- http-server/src/http_handler.cpp
+++ http-server/src/http_handler.cpp
@@ -15,10 +15,24 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-
-#include "ctx.cpp"
-
+#include <unordered_map>
+#define NEW_LINE_SIZE 2;
 #define BUF_SIZE 1024 * 8
+
+class HttpRequest {
+   public:
+    std::string path;
+    std::string method;
+    std::string protocol_version;
+    std::unordered_map<std::string, std::string> headers;
+    std::string body;
+
+    HttpRequest(HttpRequest& req) { std::cout << "copy" << '\n'; }
+
+    HttpRequest() { std::cout << "created" << '\n'; }
+
+    ~HttpRequest() { std::cout << "deleted" << '\n'; }
+};
 
 class HttpHandler {
    private:
@@ -27,27 +41,73 @@ class HttpHandler {
    public:
     HttpHandler() {}
     HttpHandler(int socket) : socket_(socket) {
-        std::cout << "create handler" << "\n";
+        std::cout << "create handler"
+                  << "\n";
+    }
+
+    HttpRequest parse(const char* buf, const int size) {
+        HttpRequest req;
+        int p = 0;
+        while (p < size && buf[p] != ' ') {
+            req.method += buf[p++];
+        }
+
+        p++;
+        while (p < size && buf[p] != ' ') {
+            req.path += buf[p++];
+        }
+
+        p++;
+        while (p < size && buf[p] != '\r') {
+            req.protocol_version += buf[p++];
+        }
+
+        p += NEW_LINE_SIZE;
+        // hedears
+        while (p < size) {
+            std::string header_name;
+            if (buf[p] == '\r') {
+                break;
+            }
+            while (p < size && buf[p] != ':') {
+                header_name += buf[p++];
+            }
+            if (header_name == "\r\n") {
+                break;
+            }
+            p += NEW_LINE_SIZE;
+            std::string header_val;
+            while (p < size && buf[p] != '\r') {
+                header_val += buf[p++];
+            }
+
+            req.headers[std::move(header_name)] = std::move(header_val);
+            p += NEW_LINE_SIZE;
+        }
+        //body
+        p += NEW_LINE_SIZE;
+        while (p < size) {
+            req.body += buf[p++];
+        }
+
+        return req;
     }
 
     void run() {
-        char buf[BUF_SIZE]; 
+        char buf[BUF_SIZE];
 
         while (true) {
-            memset(&buf, 0, sizeof(buf));
-            int read = recv(socket_, (char *)&buf, sizeof(buf), 0);
+            memset(buf, 0, sizeof(buf));
+            const int read = recv(socket_, (char*)&buf, sizeof(buf), 0);
             if (read == 0) {
                 break;
             }
-            if (buf[0] == 'q') {
-                return;
-            }
+
+            const HttpRequest req = this->parse(buf, read);
+
             std::cout << "socket got: " << buf << '\n';
         }
     }
 
-    ~HttpHandler() {
-        std::cout << "destruct" << '\n';
-        close(socket_);
-    }
+    ~HttpHandler() { close(socket_); }
 };
