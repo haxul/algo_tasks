c8bcc3fd5ea5d9a2b2954c85eed2347b44577713
haxul <s.s.starodubov@yandex.ru>
2024-05-08 21:59:27 +0400

    217

---
+++ leetcode/217.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> set;
+
+        for (int i = 0; i < ssize(nums); i++)
+        {
+            if (set.contains(nums[i])) {
+                return true; 
+            }
+
+            set.insert(nums[i]);
+        }
+        
+
+       return false; 
+    }
+};
